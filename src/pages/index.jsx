e134906764d5c3dcbde2de290001f50921c28c07
Riley Dallas <rldaggie@gmail.com>
2022-09-18 20:44:32 -0500

    Add categories to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Head from '../components/Head';
-
 import { Card } from '../components/Card'
 import { SimpleLayout } from '../components/SimpleLayout'
 
@@ -15,7 +15,8 @@ const LinkIcon = (props) => {
   )
 }
 
-const HomePage = () => {
+const HomePage = ({ data }) => {
+  const {categories} = data;
   return (
     <>
       <Head 
@@ -29,23 +30,23 @@ const HomePage = () => {
         <ul
           className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
         >
-          {[].map((project) => (
-            <Card as="li" key={project.name}>
+          {categories.nodes.map(({frontmatter: category}) => (
+            <Card as="li" key={category.title}>
               <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                 <img
-                  src={project.logo}
+                  src={`/images/logos/${category.logo}`}
                   alt=""
                   className="h-8 w-8"
                   unoptimized
                 />
               </div>
               <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <Card.Link href={project.link.href}>{project.name}</Card.Link>
+                <Card.Link href={`/${category.slug}`}>{category.title}</Card.Link>
               </h2>
-              <Card.Description>{project.description}</Card.Description>
+              <Card.Description>Topics covered: {category.topicsCovered.join(', ')} and more.</Card.Description>
               <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
                 <LinkIcon className="h-6 w-6 flex-none" />
-                <span className="ml-2">{project.link.label}</span>
+                <span className="ml-2">{`Read more...`}</span>
               </p>
             </Card>
           ))}
@@ -56,3 +57,20 @@ const HomePage = () => {
 }
 
 export default HomePage;
+
+export const pageQuery = graphql`
+query AllCategories {
+  categories: allMarkdownRemark(
+    filter: {frontmatter: {templateKey: {eq: "category"}}}
+  ) {
+    nodes {
+      frontmatter {
+        title
+        slug
+        logo
+        topicsCovered
+      }
+    }
+  }
+}
+`
--- src/pages/javascript.md
+++ src/pages/javascript.md
@@ -2,4 +2,13 @@
 templateKey: category
 title: JavaScript
 slug: javascript
+logo: js.png
+topicsCovered:
+  - Arrays
+  - Objects
+  - Strings
+  - Numbers
+  - Booleans
+  - Loops
+  - Functions
 ---
